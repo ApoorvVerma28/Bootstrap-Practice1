019465bbee665227294f1d6e34d41d7596bcc673
Apoorv Verma <143904933+ApoorvVerma28@users.noreply.github.com>
2026-01-03 23:15:24 +0530

    Add component documentation to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+// Import Home component from Pages directory
 import Home from './Pages/Home/Home';
+
+// Main App component - root component of the application
 const App = () => {
   return (
     <>
-    <Home/>
+      {/* Render the Home page component */}
+      <Home/>
     </>
   )
 }
 
+// Export App as default for use in main.jsx
 export default App
